cdeba5be8f3d5f676e4705431ec3fedf3a559289
Shaoyujie Chen <chenshaoyujie@ShaoyujieChens-MacBook-Air.local>
2023-09-14 14:00:43 -0700

    add component

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Header from './components/Header';
 
 export default function App() {
   const name = "My awesome APP"
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on {name}!</Text>
+      {/* <Text>Open up App.js to start working on {name}!</Text> */}
+      {/* use Header component and pass a prop to it with the name variable as value*/}
+      <Header appName={name}/>
       <StatusBar style="auto" />
     </View>
   );
