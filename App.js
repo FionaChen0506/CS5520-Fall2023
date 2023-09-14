251bcc0188643fd50fa07b3175e739b2b07168b8
Shaoyujie Chen <chenshaoyujie@ShaoyujieChens-MacBook-Air.local>
2023-09-14 15:06:02 -0700

    update input

--- App.js
+++ App.js
@@ -1,15 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
 import Header from './components/Header';
+import { useState } from 'react';
+import Input from './components/Input';
 
 export default function App() {
+  const [text, setText] = useState("")
   const name = "My awesome APP"
+  //update this callback function to receive the changed text and store it in state variable
+  // function changeTextHandler(changedText){
+  //   console.log(changedText);
+  //   setText(changedText);
+  // }
+
+  function changedDataHandler(data){
+    console.log("call back function called", data);
+    setText(data);
+  }
   return (
     <View style={styles.container}>
       {/* <Text>Open up App.js to start working on {name}!</Text> */}
       {/* use Header component and pass a prop to it with the name variable as value*/}
       <Header appName={name}/>
       <StatusBar style="auto" />
+      <Input changedHandler={changedDataHandler}/>
+      {/* show what user's input is */}
+      <Text>{text}</Text>
+
     </View>
   );
 }
@@ -20,5 +37,11 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+
+  input:{
+    borderColor:"lightblue",
+    borderWidth:1,
+    width: "50%",
   },
 });
---
+++ components/Input.js
@@ -0,0 +1,38 @@
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native'
+import { useState } from 'react';
+import React from 'react'
+
+const Input = ({changedHandler}) => {
+    const [text, setText] = useState("")
+    //update this callback function to receive the changed text and store it in state variable
+    function changeTextHandler(changedText){
+      console.log(changedText);
+      setText(changedText);
+    }
+
+    function confirmHandler(){
+        //call a function that is passed from app.js and pass the text via it
+        changedHandler(text);
+    }
+  return (
+    <View>
+      <TextInput
+        style={styles.input}
+        onChangeText={changeTextHandler}
+        value={text}
+        />
+
+        <Button title="confirm" onPress={confirmHandler} />
+    </View>
+  )
+}
+
+export default Input
+
+const styles = StyleSheet.create({
+    input:{
+        borderColor:"lightblue",
+        borderWidth:1,
+        width: 200,
+      },
+})
