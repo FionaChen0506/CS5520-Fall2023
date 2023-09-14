d69fb3c3231a181301e7013779dff7c22c31641b
Shaoyujie Chen <chenshaoyujie@ShaoyujieChens-MacBook-Air.local>
2023-09-14 14:12:39 -0700

    update header

--- components/Header.js
+++ components/Header.js
@@ -2,11 +2,11 @@ import { View, Text } from 'react-native'
 import React from 'react'
 
 
-const Header = (props) => {
-    console.log(props.appName)
+const Header = ({appName}) => {
+    console.log(appName)
   return (
     <View>
-      <Text>Welcome to {props.appName}</Text>
+      <Text>Welcome to {appName}</Text>
     </View>
   )
 }
