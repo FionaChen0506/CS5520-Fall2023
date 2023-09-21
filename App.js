40f098c410dde892451f507808f5e8aa8bd98d1a
Shaoyujie Chen <chenshaoyujie@ShaoyujieChens-MacBook-Air.local>
2023-09-21 14:15:33 -0700

    update modal and modal visibility

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, TextInput, View } from 'react-native';
+import { StyleSheet, Text, TextInput, View, Button } from 'react-native';
 import Header from './components/Header';
 import { useState } from 'react';
 import Input from './components/Input';
 
 export default function App() {
-  const [text, setText] = useState("")
+  const [text, setText] = useState("");
+  const [isModalVisible, setIsModalVisible] = useState(false);
   const name = "My awesome APP"
   //update this callback function to receive the changed text and store it in state variable
   // function changeTextHandler(changedText){
@@ -17,16 +18,29 @@ export default function App() {
     console.log("call back function called", data);
     setText(data);
   }
+
+  function makeModalVisible(){
+    setIsModalVisible(true)
+  }
+
+  function makeModalInvisible(){
+    setIsModalVisible(false)
+  }
   return (
     <View style={styles.container}>
       {/* <Text>Open up App.js to start working on {name}!</Text> */}
       {/* use Header component and pass a prop to it with the name variable as value*/}
       <Header appName={name}/>
       <StatusBar style="auto" />
-      <Input changedHandler={changedDataHandler}/>
-      {/* show what user's input is */}
-      <Text>{text}</Text>
 
+      <Input changedHandler={changedDataHandler}
+              modalVisibility={isModalVisible}
+              hideModal={makeModalInvisible}/>
+      <Button title="Add a goal" onPress={makeModalVisible}/>
+    
+      {/* show what user's input is */}
+      {/* this part is ovewritten by the modal temporarily */}
+      <Text>{text}</Text> 
     </View>
   );
 }
--- components/Input.js
+++ components/Input.js
@@ -1,8 +1,8 @@
-import { Button, StyleSheet, Text, TextInput, View } from 'react-native'
+import { Button, Modal, StyleSheet, Text, TextInput, View } from 'react-native'
 import { useState } from 'react';
 import React from 'react'
 
-const Input = ({changedHandler}) => {
+const Input = ({changedHandler, modalVisibility, hideModal}) => {
     const [text, setText] = useState("")
     //update this callback function to receive the changed text and store it in state variable
     function changeTextHandler(changedText){
@@ -10,6 +10,7 @@ const Input = ({changedHandler}) => {
       setText(changedText);
     }
 
+      console.log(modalVisibility)
     function confirmHandler(){
         //call a function that is passed from app.js and pass the text via it
         changedHandler(text);
@@ -17,22 +18,35 @@ const Input = ({changedHandler}) => {
         setText("")
 
     }
-  return (
-    <View>
-      <TextInput
-        style={styles.input}
-        onChangeText={changeTextHandler}
-        value={text}
-        />
 
-        <Button title="confirm" onPress={confirmHandler} />
-    </View>
+    function cancelHandler(){
+      hideModal();
+    }
+
+  return (
+    <Modal visible={modalVisibility}>
+      <View style={styles.container}>
+        <TextInput
+          style={styles.input}
+          onChangeText={changeTextHandler}
+          value={text}
+          />
+          <Button title="confirm" onPress={confirmHandler} />
+          <Button title="Cancel" onPress={cancelHandler}/>
+      </View>
+    </Modal>
   )
 }
 
 export default Input
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
     input:{
         borderColor:"lightblue",
         borderWidth:1,
