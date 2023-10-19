0fa4768cfaf9c8dd56e2172947c07a73af9660cc
Shaoyujie Chen <chenshaoyujie@ShaoyujieChens-MacBook-Air.local>
2023-10-19 14:52:16 -0700

    update firebase setup

--- firebase/FirebaseSetup.js
+++ firebase/FirebaseSetup.js
@@ -1,12 +1,12 @@
 import { getFirestore } from "firebase/firestore"
-// import{
-//     envapiKey,
-//     envauthDomain,
-//     envprojectId,
-//     envstorageBucket,
-//     envmessagingSenderId,
-//     envappId
-// } from "react-native-dotenv";
+import{
+    envapiKey,
+    authDomain,
+    projectId,
+    storageBucket,
+    messagingSenderId,
+    appId
+} from "@env";
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
@@ -14,12 +14,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCKbVc0pgAYH0J9W4Pb9l_HA5KHQBGxB0g",
-  authDomain: "cs5520-6074c.firebaseapp.com",
-  projectId: "cs5520-6074c",
-  storageBucket: "cs5520-6074c.appspot.com",
-  messagingSenderId: "455102742614",
-  appId: "1:455102742614:web:a8e4929dedd5d4266bbbf7"
+  apiKey: envapiKey,
+  authDomain: authDomain,
+  projectId: projectId,
+  storageBucket: storageBucket,
+  messagingSenderId: messagingSenderId,
+  appId: appId,
 };
 
 // Initialize Firebase
